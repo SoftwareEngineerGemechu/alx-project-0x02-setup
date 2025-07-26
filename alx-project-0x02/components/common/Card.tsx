aa0337ce8227e2389ff93c79d28a344c2392c49a
SoftwareEngineerGemechu <gemechu.bekele.berga@gmail.com>
2025-07-26 16:40:23 +0300

    Update file

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { CardProps } from "@/interfaces";
+import { type CardProps } from "@/interfaces";
 
 const Card: React.FC<CardProps> = ({ title, content }) => {
+
     return (
         <div className="bg-white rounded-xl shadow-md p-6 border border-gray-200 hover:shadow-lg transition-shadow">
         <h3 className="text-xl font-semibold text-blue-700 mb-2">{title}</h3>
